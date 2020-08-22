329a746bebb17b59f6fea7e0734ced4e77fdbad2
vitimdt <vitimdt@gmail.com>
2020-08-22 19:52:28 -0300

    Iniciando app TraderExpert

---
+++ TraderExpert/app/__init__.py
@@ -0,0 +1,79 @@
+import logging
+import os
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+''' from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_moment import Moment
+from logging.handlers import SMTPHandler, RotatingFileHandler
+'''
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+'''
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page.')
+mail = Mail()
+moment = Moment()
+'''
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    bootstrap.init_app(app)
+    '''
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+    
+    moment.init_app(app)
+    babel.init_app(app)
+    
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    if not app.debug and not app.testing and app.config['NOTIFICACAO_ERRO'] in ("email","all"):
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr=app.config['MAIL_USERNAME'],
+                toaddrs=app.config['ADMINS'], subject='Trader Expert Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+    if not app.debug and not app.testing and app.config['NOTIFICACAO_ERRO'] in ("log", "all"):
+        if not os.path.exists('log'):
+            os.mkdir('log')
+        file_handler = RotatingFileHandler('log/traderexpert.log', maxBytes=10240, backupCount=5)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog-Flask')
+    '''
+    return app
+
+from app.entities import Entities
---
+++ TraderExpert/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('STR_CONN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
