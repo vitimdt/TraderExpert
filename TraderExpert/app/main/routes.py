9fbc5450f5c38129926558f215515dad20c6643d
vitimdt <vitimdt@gmail.com>
2020-09-05 12:35:31 -0300

    Ajustando funcionalidade para manter os monitoramentos

--- TraderExpert/app/main/routes.py
+++ TraderExpert/app/main/routes.py
@@ -1,36 +1,21 @@
 from flask import render_template, request
 from app.main import bp
 from app.entities.Entities import CotacaoTempoReal, Monitoramento
-from app.helper.WebForms import WebEditableForm
 
-@bp.route('/traderexpert/', methods=['GET', 'POST'])
-@bp.route('/traderexpert/index', methods=['GET', 'POST'])
+@bp.route('/traderexpert/', methods=['GET'])
+@bp.route('/traderexpert/index', methods=['GET'])
 def index():
     cols, rs, tot = CotacaoTempoReal.buscaCotacaoTR()
     return render_template('index.html', title='Home', columns=cols, items=rs,
                            tot_invest=tot[0], tot_cotacao=tot[1], dif_total=tot[2],
                            dif_percentual=tot[3])
 
-@bp.route('/traderexpert/monitor', methods=['GET', 'POST'])
+@bp.route('/traderexpert/monitor', methods=['GET'])
 def monitor():
     cols, rs = Monitoramento.buscaMonitoramentos()
     return render_template('monitoramento.html', title='Monitoramento Ações', columns=cols, items=rs)
 
-
-@bp.route('/traderexpert/config_monitor', methods=['POST', 'GET'])
+@bp.route('/traderexpert/config_monitor', methods=['GET', 'POST'])
 def config_monitor():
-    form = WebEditableForm(request.form)
-    if (request.method == "POST") and form.validate():
-        for x in form:
-            print("POST Params: " + str(x))
-            # last_index will be set if a field is submitted
-            # if getattr(x, 'last_index', None):
-            #    model = Model1.query.get(x.last_index)
-            #    setattr(model, x.name, x.data.pop())
-            #    db.session.commit()
-    elif (request.method == "POST") and not form.validate():
-        print("Errors", form.errors)
-    monitores = Monitoramento()
-    for x in monitores.find_by_ativos():
-        print(x.acao_id, x.acao.nome)
-    return render_template('config_monitoramentos.html', title='Configurar Monitoramentos', form=form)
+    titulo = 'Configurar Monitoramento' + str(request.form['monitorSel'])
+    return render_template('configMonitoramento.html', title=titulo)
